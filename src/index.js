2510f1610efd74b937bd90dc947ea64c4ecc7e5e
TUSHAR GOYAL <tushar462024@gmail.com>
2023-04-09 14:35:11 +0530

    Standalone Buns apollo server with basic CRUD

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
   type Book {
+    id: String
     title: String
     author: String
   }
 
   type Query {
     books: [Book]
+    book(id: String): Book
+  }
+
+  type Mutation {
+    addBook(id: String, title: String, author: String): Book
   }
 `;
 
-const books = [
+let books = [
   {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    id: "1",
+    title: "The Awakening",
+    author: "Kate Chopin",
   },
   {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    id: "2",
+    title: "City of Glass",
+    author: "Paul Auster",
   },
 ];
 
 const resolvers = {
   Query: {
     books: () => books,
+    book: (_, args) => {
+      const singleBook = books.find((d) => d.id === args.id);
+      return books.find((d) => d.id === args.id);
+    },
+  },
+  Mutation: {
+    addBook: (_, { id, title, author }) => {
+      books = [...books, { id, title, author }];
+      return { id, title, author };
+    },
   },
 };
 
